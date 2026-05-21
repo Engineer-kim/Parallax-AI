a56a78d204e3d84d5e710dd20659678c63b5e2ab
Kimhanjin <ddang0103@naver.com>
2026-05-21 15:04:13 +0900

    Commit: 2차 검증 레이어 프롬프트 설정 변경

--- backend/README.md
+++ backend/README.md
@@ -2,3 +2,10 @@
 - https://cheatsheetseries.owasp.org/cheatsheets/LLM_Prompt_Injection_Prevention_Cheat_Sheet.html?utm_source=chatgpt.com
 - https://github.com/NVIDIA-NeMo/Guardrails?utm_source=chatgpt.com
 - https://guardrailsai.com/hub
+
+
+# 체크 순서
+1. Rule
+2. Groq Judge 추후 잼미니로 전환예정
+3. nemo Guardrails(NVIDIA NeMo Guardrails)
+4. 이후 로직 
--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from config import CORS_CONFIG
 from routers import test
+from routers import guard
 
 app = FastAPI(title="Parallax AI")
 app.add_middleware(CORSMiddleware, **CORS_CONFIG)
 
+
 app.include_router(test.router, prefix="/server")
+app.include_router(guard.router, prefix="/start")
