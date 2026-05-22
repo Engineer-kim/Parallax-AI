ded69a742f2a669f1fbee33b7f7a425921d61162
Kimhanjin <ddang0103@naver.com>
2026-05-22 11:46:54 +0900

    Feature: Gpt, Claude API 연동 완료

--- backend/services/llm_service.py
+++ backend/services/llm_service.py
@@ -1,22 +1,43 @@
 import os
-from openai import OpenAI
+from openai import AsyncOpenAI
 import httpx
+from openai.types.chat import ChatCompletionUserMessageParam
+from dotenv import load_dotenv
 
+load_dotenv()
 # openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 
 async def call_gpt(prompt: str):
-    # res = openai_client.chat.completions.create(
-    #     model="gpt-4o-mini",
-    #     messages=[{"role": "user", "content": prompt}]
-    # )
-    # return res.choices[0].message.content
-    return "sss"
+    key = os.getenv("GPT_KEY")
+    if not key:
+        raise ValueError(
+            "키가 설정되지 않았습니다. .env 파일을 확인하세요.(G)"
+        )
+
+    openai_client = AsyncOpenAI(api_key=key)
+
+    user_message: ChatCompletionUserMessageParam = {
+        "role": "user",
+        "content": prompt,
+    }
+
+    res = await openai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[user_message],
+        timeout=30.0,
+    )
+
+    return res.choices[0].message.content
 
 
 async def call_gemini(prompt: str):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GEMINI_API_KEY}"
+    key = os.getenv("GEMINI_API_KEY")
+    if not key:
+        raise ValueError(
+            "키가 설정되지 않았습니다. .env 파일을 확인하세요.(G)"
+        )
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={key}"
 
     payload = {
         "contents": [
@@ -39,10 +60,30 @@ async def call_gemini(prompt: str):
 
 
 async def call_claude(prompt: str):
-    # async with httpx.AsyncClient() as client:
-    #     r = await client.post(
-    #         "CLAUDE_API_ENDPOINT",
-    #         json={"prompt": prompt}
-    #     )
-    # return r.json()["text"]
-    return "sss"
+    key = os.getenv("CLAUDE_KEY")
+    if not key:
+        raise ValueError(
+            "키가 설정되지 않았습니다. .env 파일을 확인하세요.(C)"
+        )
+    url = "https://api.anthropic.com/v1/messages"
+
+    headers = {
+        "x-api-key": key,
+        "anthropic-version": "2023-06-01",
+        "content-type": "application/json",
+    }
+    payload = {
+        "model": "claude-haiku-4-5-20251001",
+        "max_tokens": 1024,
+        "messages": [{"role": "user", "content": prompt}],
+    }
+
+    async with httpx.AsyncClient(timeout=30) as client:
+        response = await client.post(url, json=payload, headers=headers)
+
+        if response.status_code != 200:
+            raise Exception(f"Claude API 에러: {response.status_code} - {response.text}")
+
+        data = response.json()
+
+    return data["content"][0]["text"]
