66bde42ca9d5aaa1b2e0c65640d82991f9c6102e
Kimhanjin <ddang0103@naver.com>
2026-05-22 17:47:02 +0900

    Update: 로그인 요청 객체에 필드 앞뒤 공백 제거 설정값 추가
     - Pydantic 설정중 필드값에 대한 공백 제거 메소드 추가

--- backend/schemas/login_request.py
+++ backend/schemas/login_request.py
@@ -3,3 +3,6 @@ from pydantic import BaseModel, Field
 class LoginRequest(BaseModel):
     login_id: str = Field(min_length=5, max_length=20)
     password: str = Field(min_length=8, max_length=16)
+
+    class Config:
+        str_strip_whitespace = True
