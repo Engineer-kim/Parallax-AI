be00fb9780ee14639b05dc6f99644a43fa21f889
Kimhanjin <ddang0103@naver.com>
2026-05-26 19:48:48 +0900

    Feature: 도커파일 및 일부 변수 환경변수에서 읽어오게끔 수정

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,12 @@
-ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALLOWED_ORIGINS = os.getenv(
+    "ALLOWED_ORIGINS",
+    "http://localhost:3000,http://127.0.0.1:3000"
+).split(",")
 
 CORS_CONFIG = {
     "allow_origins": ALLOWED_ORIGINS,
